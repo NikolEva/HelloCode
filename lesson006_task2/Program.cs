f8875764397595c14e8ce34a551ef9eb48e7e3f2
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-08-20 18:03:51 +0300

    Add task 2

---
+++ lesson006_task2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Программа, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+Console.WriteLine("input number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("input number: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("input number: ");
+int c = int.Parse(Console.ReadLine());
+int max;
+
+if(a>b)
+{
+    if(a>c)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(a);
+    }
+}
+
+else if (b>c)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+} 
+
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(c);
+}
