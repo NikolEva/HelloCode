e5f77bdce764e0a859252f3aa32b56d04f3e36b5
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-08-20 18:20:39 +0300

    Add task3

---
+++ lesson007_task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//программа, которая на вход принимает число и выдаёт, является ли число четным (делится лионо на два без остатка)
+
+Console.WriteLine("inpur number");
+int a = int.Parse(Console.ReadLine());
+if(a%2==0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
