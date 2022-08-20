04d21d01b73cc12b156c2aaee49fbb6643554149
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-08-20 18:40:40 +0300

    Add task4

---
+++ lesson008_task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа, которая на вход принимает число(N), а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("input number ");
+int a = int.Parse(Console.ReadLine());
+while(a > 0)
+{
+    if(a%2 == 0)
+    {
+        Console.WriteLine(a);
+        a = a-2;
+    }
+    else
+    {
+        a = a - 1;
+        Console.WriteLine(a);
+        a = a - 2;
+    }
+}
