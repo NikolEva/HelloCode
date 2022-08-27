9ae526d621ec7ae944fdc317ad6560f1218a2be7
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-08-27 12:59:39 +0300

    add task 23

---
+++ hw_task023/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+// например: 3 -> 1, 8, 27      5 -> 1, 8 , 27, 64, 125
+
+Console.Clear();
+Console.Write("введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write(a + " -> ");
+a = Math.Abs(a);
+
+for(int i=1; i<=a; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+}
