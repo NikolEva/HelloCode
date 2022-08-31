b0a99fbbf0884bacff040da3b9f0512f92c20329
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-08-31 20:17:28 +0300

    add task 25

---
+++ hw_task025/Program.cs
@@ -0,0 +1,17 @@
+﻿// цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+// например: 3, 5 -> 243   2, 4 -> 16
+
+Console.Clear();
+
+Console.Write("input number A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("input number B: ");
+int b = int.Parse(Console.ReadLine());
+int res = 1;
+for (int i = 1; i <= b; i++)
+{   
+    res = res * a;
+    
+}
+Console.WriteLine(a + ", " + b + " -> " + res);
