5eb58ed38e31bfda801856fba756f225eade7513
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-09-01 15:59:39 +0300

    add task 29

---
+++ hw_task029/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа, которая задает массив из 8 элементов и выводит их на экран
+
+int[] a = new int[8];
+Random b = new Random();
+Console.Write("[");
+for (int i = 0; i < 8; i++)
+{
+    a[i] = b.Next(0,1000);
+    Console.Write(a[i] + ", ");
+}
+Console.Write("]");
