a358f9c55015b908cfb2c0802f6085b18ea2df49
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-09-03 17:17:47 +0300

    add task 34

---
+++ hw_task034/Program.cs
@@ -0,0 +1,23 @@
+﻿//задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных чисел в массиве.
+// например: [345,897,568,234] -> 2     
+
+int size = 5;
+int minValue = 100;
+int maxValue = 1000;
+int count = 0;
+
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(minValue, maxValue);
+}
+
+for (int i = 0; i < size; i++)
+{
+    if(array[i]%2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(string.Join(",", array) + " -> " + count);
