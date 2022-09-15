e087fe1eb1fb17ef6f77ddff8687fab2834a93cb
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2022-09-15 15:01:05 +0300

    add task 60

---
+++ hw_tast060/Program.cs
@@ -0,0 +1,69 @@
+﻿// сформируйте трехмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+
+Console.Write("введите ширину куба ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("введите высоту куба ");
+int y = int.Parse(Console.ReadLine());
+
+Console.Write("введите глубину куба ");
+int z = int.Parse(Console.ReadLine());
+
+int[,,] GetArray(int m, int n, int p, int minValue, int maxValue)
+{
+    int[] temp = new int[m * n * p];
+    int num;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(minValue, maxValue + 1);
+        num = temp[i];
+        if(i > 0)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while(temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(minValue, maxValue + 1);
+                    j = 0;
+                    num = temp[i];
+                }
+                num = temp[i];
+            }
+        }
+    }
+
+    int count = 0;
+    int[,,] result = new int[m, n, p];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < p; k++)
+            {
+                result[i, j, k] = temp[count];
+                count++;
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+
+                Console.Write(array[i, j, k] + "(" + i + j + k + ")" + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] array3D = GetArray(x, y, z, 10, 99);
+PrintArray(array3D);
+
